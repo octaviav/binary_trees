8bca0ea1a54030cf8033c6cb3fa6044ad6a1b35f
octaviav <octavia7104@gmail.com>
2024-03-28 20:35:08 -0700

    Tree Balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		size_t left_h, right_h;
 
 		left_h = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		right = tree-> right ? 1 + binary_tree_height(tree->right) : 1;
+		right_h = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((left_h > right_h) ? left_h : right_h);
 	}
 	return (0);
